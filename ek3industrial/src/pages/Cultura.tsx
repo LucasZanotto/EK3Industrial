881d2074209119ebf7432ea10894f1a1b8490d44
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-07-28 10:32:17 -0300

    feat: mudança 2

--- ek3industrial/src/pages/Cultura.tsx
+++ ek3industrial/src/pages/Cultura.tsx
@@ -15,37 +15,37 @@ export default function Cultura(){
 </div>
 <div className="container-content">
     <div className="content-1" style={{ position: "absolute", top: "85%", left: "0%", right: "40%", transform: "translateY(-50%)" }}>
-    <h2 className="fw-bold">Cultura</h2>
-    <p>Construímos uma cultura diversa e inclusa, onde nossa equipe se sente pertencente e mais conectada.
+    <h2 className="fw-bold" style={{marginTop: "4%"}}>Cultura</h2>
+    <p style={{marginBottom:"4%"}}>Construímos uma cultura diversa e inclusa, onde nossa equipe se sente pertencente e mais conectada.
 Juntamente com a expertise técnica, temos um ambiente de trabalho mais plural, inovador e
 sustentável pois nossos funcionários são valorizados e incentivados ao empoderamento.</p>
 <p>
 Nosso propósito se torna ainda mais agradável pois focamos em <strong>4 valores:</strong>
 </p>
 <p>
-<strong>Transparência</strong> - dos planos e operações da organização para que os colaboradores possam auxiliar
+<strong style={{fontSize: "20px", font: "inherit"}}>Transparência</strong> - dos planos e operações da organização para que os colaboradores possam auxiliar
 nossos clientes a tomarem a melhor decisão em seus projetos. Realizamos de forma clara, difundindo a
 capacidade das pessoas e empresas a trabalharem melhor juntas.
 </p>
 <p>
-<strong>Autonomia</strong> – proporcionamos maior liberdade para desempenhar, inovar e trazer novas estratégias
+<strong style={{fontSize: "20px"}}>Autonomia</strong> – proporcionamos maior liberdade para desempenhar, inovar e trazer novas estratégias
 para o crescimento da empresa tanto quanto nas tomadas de decisões e suas responsabilidades;
 aplicado em todas as esferas da compania.
 </p>
 <p>
-<strong>Confiança & Colaboração</strong> – com respeito mútuo e honestidade, trabalhamos com uma comunicação
+<strong style={{fontSize: "20px"}}>Confiança & Colaboração</strong> – com respeito mútuo e honestidade, trabalhamos com uma comunicação
 livre, nutrindo um ambiente onde as equipes se sentem à vontade para compartilhar ideias e focar em
 decisões.
 </p>
 <p>
-<strong>Performance</strong> – trabalhamos de forma consistente para manter a sinergia entre equipes, além de
+<strong style={{fontSize: "20px"}}>Performance</strong> – trabalhamos de forma consistente para manter a sinergia entre equipes, além de
 cooperar para o crescimento individual e fortalecer a relação interpessoal de cada profissional.
 Resultando em um melhor desempenho coletivo, o que naturalmente gera um impacto positivo sobre a
 nossa empresa e os projetos executados. Os colaboradores e gestores têm um bom entendimento do
 que se espera deles e das competências que são avaliadas.
 </p>
-<p><strong>Nossos clientes colhem os benefícios dessa abordagem, recebendo soluções inovadoras e
-confiáveis entregues por pessoas engajadas e muito experientes.</strong></p>
+<i><p style={{marginBottom:"4%"}}><strong>Nossos clientes colhem os benefícios dessa abordagem, recebendo soluções inovadoras e
+confiáveis entregues por pessoas engajadas e muito experientes.</strong></p></i>
 </div>
 <div className="content-2" style={{paddingLeft:"65%"}}>
 <h2 className="fw-bold">Por que escolher a EK3 ?</h2>
