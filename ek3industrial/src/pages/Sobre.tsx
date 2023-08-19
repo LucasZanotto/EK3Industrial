6619f424133e1923a276c812ca4dfe9b5d2095e5
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-08-19 18:55:33 -0300

    feat: ATT20

--- ek3industrial/src/pages/Sobre.tsx
+++ ek3industrial/src/pages/Sobre.tsx
@@ -207,7 +207,7 @@ confiáveis entregues por pessoas engajadas e muito experientes.</strong></p></i
       </div>
       </div>
     <div className="mapa-sobre1" style={{backgroundColor:"#3A8894"}}>
-  <img src={imageLocalizacao} style={{objectFit:"cover"}} className="imagem-destaque" alt="Escritório" />
+  <img src={imageLocalizacao} style={{objectFit:"contain"}} className="imagem-destaque" alt="Escritório" />
 </div>
    
       <Footer />
